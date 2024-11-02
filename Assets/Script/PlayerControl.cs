74888182c014246ef888d1e8ef93353a79d6d564
sackbot32 <82033635+sackbot32@users.noreply.github.com>
2024-11-02 20:18:13 +0100

    Stamina added

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -10,25 +12,49 @@ public class PlayerControl : MonoBehaviour
     private InputActionReference jumpInput;
     [SerializeField]
     private InputActionReference lookInput;
+    [SerializeField]
+    private InputActionReference sprintInput;
+    [SerializeField]
+    private Slider sprintBar;
     private Rigidbody rb;
     private Transform cam;
+    private LayerMask layer;
+    private Transform feet;
     //Settings
     public float speed;
+    public float runMultiplier;
     public float jumpForce;
     public float vSensitivy;
     public float hSensitivity;
     public float camLimit;
+    public float detectLength;
+    public float maxSprintValue;
+    public float sprintDrainingAmmount;
+    public float sprintDrainingRate;
     //Data
     private Vector2 moveDir;
     private Vector2 lookDir;
     private float horRot;
     private float verRot;
-
+    private bool canJump;
+    private float trueSpeed;
+    private float currentSprintValue;
+    private bool sprinting;
+    private bool isDrainingSprint;
+    private bool canSprint;
     private void Start()
     {
+        canSprint = true;
+        sprinting = false;
+        currentSprintValue = maxSprintValue;
+        sprintBar.maxValue = maxSprintValue;
+        sprintBar.value = currentSprintValue;
+        layer = LayerMask.GetMask("Ground");
+        canJump = false;
         rb = GetComponent<Rigidbody>();
         cam = Camera.main.transform;
         Cursor.lockState = CursorLockMode.Locked;
+        feet = transform.GetChild(2).transform;
     }
 
     // Update is called once per frame
@@ -36,9 +62,15 @@ public class PlayerControl : MonoBehaviour
     {
         moveDir = moveInput.action.ReadValue<Vector2>().normalized;
         lookDir = lookInput.action.ReadValue<Vector2>();
-        if (jumpInput.action.WasCompletedThisFrame())
+        DetectGround();
+        if (jumpInput.action.WasPressedThisFrame() && canJump)
         {
             Jump();
+        }
+        sprinting = sprintInput.action.IsInProgress() && canSprint;
+        if(sprinting && !isDrainingSprint && canSprint)
+        {
+            StartCoroutine(DrainWhileSprinting());
         }
         Look();
 
@@ -51,7 +83,8 @@ public class PlayerControl : MonoBehaviour
 
     private void Move()
     {
-        rb.linearVelocity = (transform.forward * moveDir.y + transform.right*moveDir.x)*speed;
+        trueSpeed = sprinting ? speed * runMultiplier: speed;
+        rb.linearVelocity = (transform.forward * moveDir.y + transform.right*moveDir.x)*trueSpeed + transform.up*rb.linearVelocity.y;
     }
 
     private void Jump()
@@ -70,5 +103,71 @@ public class PlayerControl : MonoBehaviour
         transform.Rotate(Vector3.up * horRot);
         cam.localRotation = Quaternion.Euler(verRot, 0f, 0f);
     }
+
+    private void DetectGround()
+    {
+        Debug.DrawRay(feet.position, Vector3.down * detectLength,Color.red);
+        if (Physics.Raycast(feet.position,Vector3.down,detectLength,layer))
+        {
+            canJump = true;
+        } else
+        {
+            canJump = false;
+        }
+    }
+
+    private IEnumerator DrainWhileSprinting()
+    {
+        isDrainingSprint = true;
+        while(sprinting && (currentSprintValue > 0))
+        {
+            currentSprintValue -= sprintDrainingAmmount;
+            sprintBar.value = currentSprintValue;
+            yield return new WaitForSeconds(sprintDrainingRate);
+        }
+        
+        if(currentSprintValue > 0)
+        {
+            StartCoroutine(NormalSprintRegen());
+        } else
+        {
+            StartCoroutine(EmptySprintRegen());
+        }
+    }
+
+    private IEnumerator NormalSprintRegen()
+    {
+        isDrainingSprint = false;
+        print("Con estamina");
+        while (!sprinting && (currentSprintValue < maxSprintValue))
+        {
+            currentSprintValue += sprintDrainingAmmount;
+            sprintBar.value = currentSprintValue;
+            yield return new WaitForSeconds(sprintDrainingRate);
+        }
+        if(currentSprintValue > maxSprintValue)
+        {
+            currentSprintValue = maxSprintValue;
+        }
+    }
+
+    private IEnumerator EmptySprintRegen()
+    {
+        canSprint = false;
+        print("Sin estamina");
+        while ((currentSprintValue < maxSprintValue))
+        {
+            currentSprintValue += sprintDrainingAmmount/2;
+            sprintBar.value = currentSprintValue;
+            yield return new WaitForSeconds(sprintDrainingRate);
+        }
+        if (currentSprintValue > maxSprintValue)
+        {
+            currentSprintValue = maxSprintValue;
+        }
+        isDrainingSprint = false;
+        canSprint = true;
+    }
+
 
 }
